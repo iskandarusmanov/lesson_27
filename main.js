7ecd68814b1dcc380c7f2275fcdb379ffd829352
iskandarusmanov <usman29104@gmail.com>
2023-10-11 18:42:24 +0500

    done

---
+++ main.js
@@ -0,0 +1,57 @@
+//for_1
+
+// let k = 45
+// let n = 6
+// for (let i = 0; i < n; i++) {
+//     console.log(k);
+// }
+
+
+
+//for_2
+
+// let a = parseInt(prompt("A ni kiriting (a < b) "))
+// let b = parseInt(prompt("B ni kiriting (a < b)"))
+// for (let i = a; i <= b; i++) {
+//     alert(i)
+// }
+
+
+
+//for_3
+
+// let a = parseInt(prompt("A ni kiriting (a < b) "))
+// let b = parseInt(prompt("B ni kiriting (a < b)"))
+// for (let i = b-1; i > a; i--) {
+//     alert(i)
+// }
+
+
+
+//for_4
+
+// let a = parseInt(prompt("Konfet narxini  kiriting"))
+// for (let i = a; i <= a * 10; i = i + a) {
+//     alert(i)
+// }
+
+
+
+//for_5
+
+// let a = parseInt(prompt("Konfet narxini  kiriting"))
+// for (let i = a; i <= a * 2; i = i + a/10) {
+//     alert(i)
+// }
+
+
+
+//for_6
+
+let a = parseInt(prompt("Konfet narxini  kiriting"))
+for (let i = 1.2 ; i <= 2; i = i + 0.1) {
+    if ((i * 10) % 2 !== 0) {
+        continue;
+    }
+    alert(i*a + " UZS")
+}
